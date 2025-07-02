b2fc3fe34236fd406c519fa0be06bc334fd50451
Loicrogue <loic.hoarau2004@gmail.com>
2025-07-02 23:27:35 +0200

    add responsivity

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-
 import { useTranslation } from 'react-i18next';
+import useResponsive from '../hooks/useResponsive';
 
 const Home = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const { isMobile, isTablet, isDesktop } = useResponsive();
 
-    return (
-        <div className="fixed inset-0 bg-background-breezy overflow-auto">
-            <div className="absolute inset-0 bg-container-breezy shadow-xl rounded-xl m-5">
-                <h1 className="text-2xl font-bold text-center mt-4">{t('pages.home.title')}</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`
+        fixed inset-0 overflow-auto
+        ${isMobile ? 'bg-background-breezy' : ''}
+        ${isTablet ? 'bg-blue-400' : ''}
+        ${isDesktop ? 'bg-green-400' : ''}
+      `}
+    >
+      <div className={`
+        absolute inset-0 shadow-xl rounded-xl m-5
+        ${isMobile ? 'bg-container-breezy' : ''}
+        ${isTablet ? 'bg-white' : ''}
+        ${isDesktop ? 'bg-gray-50' : ''}
+      `}>
+        <h1 className={`font-bold text-center mt-4 
+            ${isMobile ? 'text-xs' : ''} 
+            ${isTablet ? 'text-md' : ''} 
+            ${isDesktop ? 'text-3xl' : ''}`}
+        >
+            {t('pages.home.title')}
+        </h1>
+        {isMobile && <p className="text-center text-sm text-gray-500">Affichage Mobile</p>}
+        {isTablet && <p className="text-center text-sm text-gray-500">Affichage Tablette</p>}
+        {isDesktop && <p className="text-center text-sm text-gray-500">Affichage PC</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
