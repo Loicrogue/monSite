8dc602956fd88696b82c00bb24c8eea142334741
Loicrogue <loic.hoarau2004@gmail.com>
2025-09-04 22:04:41 +0200

    small ajustements on aboutMe page

--- front/src/components/aboutMeComponent.tsx
+++ front/src/components/aboutMeComponent.tsx
@@ -6,22 +6,30 @@ const AboutMeComponent: React.FC = () => {
     const { t } = useTranslation();
     
     return (
-        <div className='flex flex-col w-full items-center text-monSite p-5'>
+        <div className='flex flex-col w-full items-center text-monSite p-5 gap-5'>
             <motion.div 
 				initial={{ opacity: 0, x: -500 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8, ease: "easeOut" }}
-				className="justify-items-center w-full bg-red-500 py-8"
+				className="justify-items-center w-full"
 			>
-                <span>a</span>
+                <span className='flex w-full justify-center'>{t('pages.notFound.workInProgress')}</span>
             </motion.div>
             <motion.div 
 				initial={{ opacity: 0, x: 500 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8, ease: "easeOut", delay: 0.5 }}
-				className="justify-items-center flex w-full bg-blue-500 py-8"
+				className="justify-items-center flex w-full"
 			>
-                <span>a</span>
+                <span className='flex w-full justify-center'>{t('pages.notFound.workInProgress')}</span>
+            </motion.div>
+            <motion.div 
+				initial={{ opacity: 0, x: -500 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8, ease: "easeOut", delay: 1 }}
+				className="justify-items-center flex w-full"
+			>
+                <span className='flex w-full justify-center'>{t('pages.notFound.workInProgress')}</span>
             </motion.div>
         </div>
     );
