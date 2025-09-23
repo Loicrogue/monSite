bacaa196795efd566494ce9149520600fcd2306b
Loicrogue <loic.hoarau2004@gmail.com>
2025-09-23 23:05:08 +0200

    refactor traduction for interests content

--- front/src/components/interestsList/Cybersecurity.tsx
+++ front/src/components/interestsList/Cybersecurity.tsx
@@ -4,20 +4,12 @@ import { useTranslation } from 'react-i18next';
 
 const Cybersecurity = () => {
   const { t } = useTranslation();
-  
+
   return (
     <div>
       <h2 className="font-bold text-xl mb-3">🛡️ {t("pages.interests.bookmarks.cybersecurity")}</h2>
-      <p className="mb-2">
-        La cybersécurité est pour moi un domaine essentiel : comprendre comment
-        protéger les systèmes, anticiper les menaces et analyser les failles
-        potentielles.
-      </p>
-      <p>
-        Mon objectif est de toujours renforcer ma capacité à
-        penser comme un attaquant pour mieux sécuriser mes développements, 
-        ainsi que mes activités personnelles.
-      </p>
+     <p className="mb-2">{t("pages.interests.contents.cybersecurity.1")}</p>
+      <p>{t("pages.interests.contents.cybersecurity.2")}</p>
     </div>
   );
 };
--- front/src/components/interestsList/HardSoft.tsx
+++ front/src/components/interestsList/HardSoft.tsx
@@ -4,21 +4,12 @@ import { useTranslation } from 'react-i18next';
 
 const HardSoft = () => {
   const { t } = useTranslation();
-  
+
   return (
     <div>
       <h2 className="font-bold text-xl mb-3">🖥️ {t("pages.interests.bookmarks.hardSoft")}</h2>
-      <p className="mb-2">
-        Comprendre l’interaction entre le matériel et le logiciel est une
-        passion. J’aime explorer l’architecture des composants, assembler et
-        optimiser des configurations, tout en étudiant comment les systèmes
-        d’exploitation exploitent les ressources disponibles.
-      </p>
-      <p>
-        Cela me permet de mieux appréhender les performances, les
-        compatibilités, et d’avoir une vision complète du fonctionnement d’un
-        ordinateur, de la couche matérielle jusqu’aux applications.
-      </p>
+      <p className="mb-2">{t("pages.interests.contents.hardSoft.1")}</p>
+      <p>{t("pages.interests.contents.hardSoft.2")}</p>
     </div>
   );
 };
