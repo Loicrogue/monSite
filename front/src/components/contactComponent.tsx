75061f741b4f928cf6c52c1789957e772b12b7ad
Loicrogue <loic.hoarau2004@gmail.com>
2026-01-17 20:12:18 +0100

    correction url contact-success

--- front/src/components/contactComponent.tsx
+++ front/src/components/contactComponent.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import useResponsive from '../hooks/useResponsive';
 
 const ContactComponent: React.FC = () => {
-  const { t } = useTranslation();
+  const { t, i18n  } = useTranslation();
   const { isMobile, isTablet } = useResponsive();
   const textSizeClass = isMobile || isTablet ? 'text-l' : 'text-xl';
 
@@ -16,12 +16,12 @@ const ContactComponent: React.FC = () => {
       <form
         name="contact"
         method="POST"
-        action="contact-success"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         className="flex flex-col gap-4"
       >
         <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="redirect" value={`/${i18n.language}/contact-success`} />
 
         {/* Honeypot anti-spam */}
         <p hidden>
