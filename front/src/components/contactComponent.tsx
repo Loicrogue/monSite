7be13cab1975befaf37e4789394a8581ed560bd4
Loicrogue <loic.hoarau2004@gmail.com>
2026-01-17 20:37:24 +0100

    correction

--- front/src/components/contactComponent.tsx
+++ front/src/components/contactComponent.tsx
@@ -4,6 +4,7 @@ import useResponsive from '../hooks/useResponsive';
 
 const ContactComponent: React.FC = () => {
   const { t } = useTranslation();
+  const lang = window.location.pathname.split('/')[1] || 'fr';
   const { isMobile, isTablet } = useResponsive();
   const textSizeClass = isMobile || isTablet ? 'text-l' : 'text-xl';
 
@@ -16,7 +17,7 @@ const ContactComponent: React.FC = () => {
       <form
         name="contact"
         method="POST"
-        action="contact-success"
+        action={`/${lang}/contact-success`}
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         className="flex flex-col gap-4"
