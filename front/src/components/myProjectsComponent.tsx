03d22199c9f745ffcc509854a20cda794c9dbf71
Loicrogue <loic.hoarau2004@gmail.com>
2026-02-03 21:58:54 +0100

    correction style and motion

--- front/src/components/myProjectsComponent.tsx
+++ front/src/components/myProjectsComponent.tsx
@@ -62,7 +62,7 @@ const MyProjectsComponent: React.FC = () => {
             key={index}
             initial={{ opacity: 0, x: index % 2 == 0 ? -500 : 500 }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut", delay: index * 0.35 }}
+            transition={{ duration: 0.3, ease: "easeOut", delay: index * 0.15 }}
             className="flex p-4 cursor-pointer hover:scale-103 transition-transform items-center justify-center flex-col bg-child-child-container-monSite rounded-lg shadow-lg"
             onClick={() => {
               setSelectedProject(item);
